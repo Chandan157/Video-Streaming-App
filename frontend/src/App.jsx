201dbf00a733f2469adabd7a9f3c0eafb4b14c2d
Chandan <94438598+Chandan157@users.noreply.github.com>
2024-07-10 10:26:19 +0530

    video streaming concept

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import VideoPlayer from "./VideoPlayer";
+import { useRef } from "react";
+
+function App() {
+  const playerRef = useRef(null);
+  const videoLink =
+    "https://localhost:8000/uploads/courses/f857b83d-c25d-4677-87cb-8d3fd4b0fa33/index.m3u8";
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // We can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+  return (
+    <>
+      <div>
+        <h1>Video player</h1>
+      </div>
+      <VideoPlayer options={videoPlayerOptions} onReady={handlePlayerReady} />
+    </>
+  );
+}
+
+export default App;
